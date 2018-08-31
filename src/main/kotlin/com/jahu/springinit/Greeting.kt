87d16727ddb65bdc26c33d4ba803bf41541c44b6
Jan Hudzik <jan.hudzik@gmail.com>
2018-08-31 16:05:49 +0200

    Implementation of GreetingController handling requests to /greeting endpoint
    http://spring.io/guides/gs/rest-service/

---
+++ src/main/kotlin/com/jahu/springinit/Greeting.kt
@@ -0,0 +1,9 @@
+package com.jahu.springinit
+
+/**
+* Created by jahudzik on 30/08/2018.
+*/
+data class Greeting(
+        val id: Long,
+        val content: String
+)
---
+++ src/main/kotlin/com/jahu/springinit/GreetingController.kt
@@ -0,0 +1,22 @@
+package com.jahu.springinit
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+/**
+ * Created by jahudzik on 30/08/2018.
+ */
+@RestController
+class GreetingController {
+
+    private val template = "Hello, %s"
+    private val counter = AtomicLong()
+
+    @RequestMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String): Greeting {
+        return Greeting(counter.incrementAndGet(), String.format(template, name))
+    }
+
+}
